f83a00a1b736d387bce7f7b995087b837cc6059b
pdhakal906 <71127745+pdhakal906@users.noreply.github.com>
2024-09-06 14:20:41 +0545

    Update mailer.js

--- mailer.js
+++ mailer.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
-  host: 'stmp.gmail.com',
+  host: 'stmp.gmail.com', // may be smtp.gmail.com
   port: 587,
   secure: false,
   auth: {
